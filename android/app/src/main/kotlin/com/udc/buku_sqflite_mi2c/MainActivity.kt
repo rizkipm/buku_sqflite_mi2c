a2755c5c9f6cf053693034580ef1dad3a4a0da23
Rizki Pm <udarizki2020@gmail.com>
2025-05-16 15:48:53 +0800

    update

---
+++ android/app/src/main/kotlin/com/udc/buku_sqflite_mi2c/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.udc.buku_sqflite_mi2c
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/model/model_buku.dart
@@ -0,0 +1,11 @@
+class ModelBuku{
+
+  final int? id;
+  final String namaBuku;
+  final String judulBuku;
+
+  ModelBuku({this.id, required this.namaBuku, required this.judulBuku});
+
+
+
+}
